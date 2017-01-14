e11ef410e57458afdebc978f37b888a99df67c6b
piyushhajare <piyushhajare@gmail.com>
2017-01-14 11:27:44 +0530

    Adds many to one relationship

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Employee(models.Model):
 	author = models.ForeignKey('auth.User')
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
 	pay_per_month = models.CharField(max_length=20)
-	created_date = models.DateTimeField(default=timezone.now)
+	
 
 	def publish(self):
 		self.save()
@@ -19,7 +20,10 @@ class Month(models.Model):
 		date = models.DateTimeField(default=timezone.now)
 		no_of_holidays = models.CharField(max_length=10)
 		total_advance_overhead = models.CharField(max_length=15)
-
+		Employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
 
 		def get_month(self):
-			month = self.date.month				
+			month = self.date.month
+		
+		def __str__(self):
+			return str(self.date.month)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Employee
+from .models import Month
+
+def wage_list(request):
+	return render(request,'app/wage_list.html',{})
 
 # Create your views here.
+wage_list
